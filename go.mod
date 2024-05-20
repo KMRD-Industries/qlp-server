92665761e1be2a023f98baa45ef6f561f11becbe
Mikołaj Siatkowski <msiatkowski@tutanota.com>
2024-05-20 15:28:16 +0200

    setup for real multiplayer

--- go.mod
+++ go.mod
@@ -3,6 +3,6 @@ module server
 go 1.22.1
 
 require (
-	github.com/kmrd-industries/qlp-proto-bindings v0.1.1-0.20240423230031-2f703a0214c4
+	github.com/kmrd-industries/qlp-proto-bindings v0.1.1-0.20240520115533-bb2d3ce12894
 	google.golang.org/protobuf v1.33.0
 )
--- main.go
+++ main.go
@@ -1,10 +1,10 @@
 package main
 
 import (
+	"errors"
 	"log"
 	"net"
 	"net/netip"
-	"slices"
 	"time"
 
 	pb "github.com/kmrd-industries/qlp-proto-bindings/gen/go"
@@ -17,14 +17,12 @@ const (
 )
 
 var (
-	ip = net.ParseIP("127.0.0.1")
-	// addrPorts = make(map[uint32]*net.UDPAddr, 32)
-	addrPorts = make([]netip.AddrPort, 32)
+	ip        = net.ParseIP("127.0.0.1")
+	addrPorts = make(map[uint32]netip.AddrPort, 32)
 	tcp_conns = make(map[uint32]net.Conn, 32)
 )
 
-// for now only for sending ids
-func handleTCP() {
+func listenTCP() {
 	addr := net.TCPAddr{
 		IP:   ip,
 		Port: SERVER_PORT,
@@ -38,24 +36,59 @@ func handleTCP() {
 	}
 	defer listener.Close()
 
-	var id uint32 = 0
-	for id = 0; ; id++ {
+	var id uint32 = 1
+	for ; ; id++ {
 		conn, err := listener.AcceptTCP()
 
 		if err != nil {
 			log.Printf("Failed to accept tcp connection: %v\n", err)
 		} else {
 			// inform player of received id
-			msg := &pb.ConnectionReply{
-				Id: id,
+			msg := &pb.StateUpdate{
+				Id:      id,
+				Variant: pb.StateVariant_CONNECTED,
 			}
 			encoded, _ := proto.Marshal(msg)
 
 			conn.Write(encoded)
 			// these will be monitored (we're assuming that closing conn means losing connection)
 			tcp_conns[id] = conn
-		}
 
+			go handleTCP(id, conn)
+		}
+	}
+}
+
+// for now only for sending ids
+func handleTCP(id uint32, conn *net.TCPConn) {
+	b := make([]byte, BUF_SIZE)
+	defer conn.Close()
+
+	msg := &pb.StateUpdate{
+		Variant: pb.StateVariant_CONNECTED,
+	}
+
+	for otherID, c := range tcp_conns {
+		msg.Id = otherID
+		encoded, _ := proto.Marshal(msg)
+		c.Write(encoded)
+	}
+
+	for {
+		conn.SetReadDeadline(time.Now().Add(2000 * time.Millisecond))
+		_, err := conn.Read(b)
+
+		if errors.Is(err, net.ErrClosed) {
+			msg.Id = id
+			msg.Variant = pb.StateVariant_DISCONNECTED
+
+			for _, c := range tcp_conns {
+				encoded, _ := proto.Marshal(msg)
+				c.Write(encoded)
+			}
+			delete(tcp_conns, id)
+			break
+		}
 	}
 }
 
@@ -87,25 +120,18 @@ func handleUDP() {
 				log.Printf("Failed to deserialize: %v\n", err)
 				continue
 			}
-			// log.Printf("%v\n", received)
-
-			// id := received.EntityId
-
-			// if _, ok := udp_addrs[id]; !ok {
-			// 	udp_addrs[id] = sender
-			// }
 
 			senderAddrPort := sender.AddrPort()
-			if !slices.Contains(addrPorts, senderAddrPort) {
-				addrPorts = append(addrPorts, senderAddrPort)
+			id := received.EntityId
+			if val, ok := addrPorts[id]; !ok || val != senderAddrPort {
+				addrPorts[id] = senderAddrPort
 			}
 
 			// pass update to other players
-			for _, addrPort := range addrPorts {
-				// if id != otherID {
-				if senderAddrPort != addrPort {
-					udp_addr := net.UDPAddrFromAddrPort(addrPort)
-					conn.WriteToUDP(b[:n], udp_addr)
+			for otherID, addrPort := range addrPorts {
+				if otherID != id {
+					udpAddr := net.UDPAddrFromAddrPort(addrPort)
+					conn.WriteToUDP(b[:n], udpAddr)
 				}
 			}
 		}
@@ -113,8 +139,8 @@ func handleUDP() {
 }
 
 func main() {
-	go handleTCP()
 	go handleUDP()
+	go listenTCP()
 
 	for {
 	}
